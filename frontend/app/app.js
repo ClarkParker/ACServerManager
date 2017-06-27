2239d896479c988e6e9a55f7d8a149bee13882fa
ClarkParker <Devishmend@gmx.net>
2017-06-27 08:51:59 +0200

    copied from Pringelz

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,4 +1,18 @@
 'use strict';
 
 angular
-	.module('acServerManager', ['acServerManager.services', 'ui.bootstrap', 'ui.bootstrap.showErrors'])
+	.module('acServerManager', ['acServerManager.services', 'ui.bootstrap', 'ui.bootstrap.showErrors', 'toggle-switch'])
+	
+.directive('stringToNumber', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$parsers.push(function(value) {
+        return '' + value;
+      });
+      ngModel.$formatters.push(function(value) {
+        return parseFloat(value, 10);
+      });
+    }
+  };
+});
